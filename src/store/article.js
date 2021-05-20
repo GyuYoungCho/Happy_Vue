a14c89f3ec094b8a284011ab9a860ddb8bf653e7
sunhye <wkadnsj@gmail.com>
2021-05-20 17:58:40 +0900

    'login'

---
+++ src/store/article.js
@@ -0,0 +1,59 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import rest from "@/js/httpCommon.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    items: [],
+    item: {},
+  },
+  getters: {
+    items(state) {
+      return state.items;
+    },
+    item(state) {
+      return state.item;
+    },
+  },
+  mutations: {
+    setItems(state, payload) {
+      state.items = payload;
+    },
+    setItem(state, payload) {
+      state.item = payload;
+    },
+  },
+  actions: {
+    setItems(store) {
+      rest
+        .axios({
+          method: "get",
+          url: "/article",
+        })
+        .then((res) => {
+          store.commit("setItems", res.data);
+        })
+        .catch((err) => {
+          alert("게시판 로딩 실패");
+          console.log(err);
+        });
+    },
+    setItem(store, num) {
+      rest
+        .axios({
+          method: "get",
+          url: "/article/" + num,
+        })
+        .then((res) => {
+          store.commit("setItem", res.data);
+        })
+        .catch((err) => {
+          alert("상세화면 로딩 실패");
+          console.log(err);
+        });
+    },
+  },
+  modules: {},
+});
