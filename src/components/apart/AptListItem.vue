439c004f3bf62132875453be19dac6ceb550671d
GyuYoungCho <rbud613@gmail.com>
2021-05-20 17:59:32 +0900

    commit

---
+++ src/components/apart/AptListItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <b-row
+    class="m-2"
+    @click="chooseApt"
+    @mouseover="colorChange(true)"
+    @mouseout="colorChange(false)"
+    :class="{ 'mouse-over-bgcolor': isColor }"
+  >
+    
+    <b-col cols="10">
+      {{ this.apt.아파트 }}
+      <h6>거래금액 : {{this.apt.거래금액}}</h6>
+      <h6>면적 : {{this.apt.면적}}</h6>
+      <p>{{this.apt.년}}.{{this.apt.월}}.{{this.apt.일}}</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'AptListItem',
+  data() {
+    return {
+      isColor: false,
+    };
+  },
+  props: {
+    apt: Object,
+  },
+  methods: {
+    // chooseApt() {
+    //   // this.$emit('select-apt', this.apt);
+    //   this.$store.dispatch('selectApt', this.apt);
+    // },
+    ...mapActions(['selectApt']),
+    chooseApt() {
+      this.selectApt(this.apt);
+    },
+    colorChange(flag) {
+      this.isColor = flag;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.img-list {
+  width: 50px;
+}
+.mouse-over-bgcolor {
+  background-color: lightblue;
+}
+</style>
