81417cc72bd2f8ec385d2f4e66c54cd9f189aae9
sunhye <wkadnsj@gmail.com>
2021-05-26 22:26:39 +0900

    qnadetail

--- src/components/views/QnaDetail.vue
+++ src/components/views/QnaDetail.vue
@@ -14,105 +14,94 @@
                 <b-button @click="del(qnai.num)" variant="outline-danger">삭제</b-button>
                 <b-button @click="moveList" vari ant="outline-success">목록</b-button>
 
-            <b-form-group label="내용" label-for="content">
-                <b-form-textarea id="qcontent" rows="5" placeholder="content" readonly v-model="qnai.content"></b-form-textarea>
-            </b-form-group>
-            <b-form inline>
-                <b-col v-if="user.id==qnai.id" class="ml-auto">
-                    <b-button @click="qnamodOpen()" variant="outline-primary">수정</b-button>
-                    <b-button v-b-modal.qna-del-modal variant="outline-danger">삭제</b-button>
-                </b-col>
-                <b-button @click="moveList" variant="outline-success">목록</b-button>
-                
+                <b-form-group label="내용" label-for="content">
+                    <b-form-textarea id="qcontent" rows="5" placeholder="content" readonly v-model="qnai.content"></b-form-textarea>
+                </b-form-group>
+                <b-form inline>
+                    <b-col v-if="user.id==qnai.id" class="ml-auto">
+                        <b-button @click="qnamodOpen()" variant="outline-primary">수정</b-button>
+                        <b-button v-b-modal.qna-del-modal variant="outline-danger">삭제</b-button>
+                    </b-col>
+                    <b-button @click="moveList" variant="outline-success">목록</b-button>
+                </b-form>
+
+                <b-card no-body class="mt-5">
+                    <b-list-group flush>
+                        <b-list-group-item v-for="(cmt, idx) in comitems" :key="idx">
+                            <b-row>
+                                <b-col cols="2">
+                                    <b-badge>{{ cmt.name }}</b-badge>
+                                </b-col>
+                                <b-col cols="6">
+                                    <span>  {{ cmt.comment}}</span>
+                                </b-col>
+                                <b-col cols="2">
+                                    <span>  {{ cmt.date |formatDate}}</span>
+                                </b-col>
+                                <!-- <b-col v-if="user && user.id == comi.id" cols="2"> -->
+                                <b-button-group class="float-right" size="sm">
+                                    <b-btn variant="warning" @click="commodOpen(cmt)"><b-icon icon="pencil"></b-icon></b-btn>
+                                    <b-btn variant="danger" @click="comdel(cmt)"><b-icon icon="trash"></b-icon></b-btn>
+                                </b-button-group>
+                                <!-- </b-col> -->
+                            </b-row>
+                        </b-list-group-item>
+                    </b-list-group>
+                    <b-card-footer inline>
+                        <b-textarea v-model="comform.comment"></b-textarea>
+                        <b-btn variant="outline-success" class="float-right" @click="comregist">댓글 등록</b-btn>
+                    </b-card-footer>
+                </b-card>
             </b-form>
-        <b-card no-body class="mt-5">
-
-          <b-list-group flush>
-            <b-list-group-item v-for="(cmt, idx) in comitems" :key="idx">
-              <b-row>
-                <b-col cols="2">
-                  <b-badge>{{ cmt.name }}</b-badge>
-                </b-col>
-                <b-col cols="6">
-                  <span>  {{ cmt.comment}}</span>
-                </b-col>
-                <b-col cols="2">
-                  <span>  {{ cmt.date |formatDate}}</span>
-                </b-col>
-                <!-- <b-col v-if="user && user.id == comi.id" cols="2"> -->
-                  <b-button-group class="float-right" size="sm">
-                    <b-btn variant="warning" @click="commodOpen(cmt)"><b-icon icon="pencil"></b-icon></b-btn>
-                    <b-btn variant="danger" @click="comdel(cmt)"><b-icon icon="trash"></b-icon></b-btn>
-                  </b-button-group>
-                <!-- </b-col> -->
-              </b-row>
 
 
-            </b-list-group-item>
-          </b-list-group>
-            <b-card-footer inline>
-            <b-textarea v-model="comform.comment"></b-textarea>
-            <b-btn variant="outline-success" class="float-right" @click="comregist">댓글 등록</b-btn>
-          </b-card-footer>
-        </b-card>
-        
-        </b-form>
+            <b-modal ref="qnamod" hide-footer title="글 수정하기">
+                <b-form >
+                    <b-form-group label="제목:" label-for="ftitle">
+                        <b-form-input id="ftitle"
+                            type="text"
+                            v-model="qnaform.title"
+                            required
+                            placeholder="제목">
+                        </b-form-input>
+                    </b-form-group>
+
+                    <b-form-group label="글" label-for="fcontent">
+                        <b-form-textarea id="fcontent"
+                            v-model="qnaform.content"
+                            placeholder="글쓰기..."
+                            :rows="10"
+                            :max-rows="20">
+                        </b-form-textarea>
+                    </b-form-group>
+                    <b-button @click="qnamodify()" variant="warning" class="float-right">글 수정</b-button>
+                </b-form>
+            </b-modal>
 
 
-    <b-modal ref="qnamod" hide-footer title="글 수정하기">
-      <b-form >
+            <b-modal ref="commod" hide-footer title="댓글 수정하기">
+                <b-form>
+                    <b-form-group label="글" label-for="comment">
+                        <b-form-textarea id="comment"
+                            v-model="modcomment"
+                            placeholder="글쓰기..."
+                            :rows="10"
+                            :max-rows="20">
+                        </b-form-textarea>
+                    </b-form-group>
+                    <b-btn @click="commodify()" variant="warning" class="float-right">글 수정</b-btn>
+                </b-form>
+            </b-modal>
 
-        <b-form-group label="제목:"
-                      label-for="ftitle">
-          <b-form-input id="ftitle"
-                        type="text"
-                        v-model="qnaform.title"
-                        required
-                        placeholder="제목">
-          </b-form-input>
-        </b-form-group>
-
-        <b-form-group label="글"
-                      label-for="fcontent">
-          <b-form-textarea id="fcontent"
-                           v-model="qnaform.content"
-                           placeholder="글쓰기..."
-                           :rows="10"
-                           :max-rows="20">
-          </b-form-textarea>
-        </b-form-group>
-
-        <b-button @click="qnamodify()" variant="warning" class="float-right">글 수정</b-button>
-      </b-form>
-    </b-modal>
-
-
-    <b-modal ref="commod" hide-footer title="댓글 수정하기">
-      <b-form>
-
-        <b-form-group label="글"
-                      label-for="comment">
-          <b-form-textarea id="comment"
-                           v-model="modcomment"
-                           placeholder="글쓰기..."
-                           :rows="10"
-                           :max-rows="20">
-          </b-form-textarea>
-        </b-form-group>
-
-        <b-btn @click="commodify()" variant="warning" class="float-right">글 수정</b-btn>
-      </b-form>
-    </b-modal>
-
-    <b-modal id="qna-del-modal" hide-footer title="글 삭제">
-      <div class="d-block text-center">
-        <h3>삭제하시겠습니까?</h3>
-      </div>
-      <b-button class="mt-2" variant="outline-warning" block @click="qnadel()">예</b-button>
-      <b-button class="mt-3" variant="outline-secondary" block @click="qnahideModal">아니요</b-button>
-    </b-modal>
-
-    </div>
+            <b-modal id="qna-del-modal" hide-footer title="글 삭제">
+                <div class="d-block text-center">
+                    <h3>삭제하시겠습니까?</h3>
+                </div>
+                <b-button class="mt-2" variant="outline-warning" block @click="qnadel()">예</b-button>
+                <b-button class="mt-3" variant="outline-secondary" block @click="qnahideModal">아니요</b-button>
+            </b-modal>
+        </div>
+    </b-container>
 </template>
 <style scoped>
 
